1d3694f9f9ceea301ff9cf038e50e5ae9925a44d
Reddash <44251376+Reddash@users.noreply.github.com>
2018-10-18 14:37:28 +1100

    Update runnertest.sh

--- runnertest.sh
+++ runnertest.sh
@@ -2,4 +2,4 @@
 
 export APP_ENV=${1:-dev4}
 export API_NAME${2}
-./node_modules/mocha/bin/_mocha test/tests/ --reporter mochawesome --reporter-options reportDir=E2EReports,reportFilename=E2EReport -t 10000 --grep @dev4
+./node_modules/mocha/bin/_mocha test/tests/ --reporter mochawesome --reporter-options reportDir=TestReport,reportFilename=TestReport -t 10000 --grep @regression
